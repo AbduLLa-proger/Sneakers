ef33892c186daa553fad7e0b25031fb127f86bf4
AbduLLa <43612762+u1810211@users.noreply.github.com>
2021-12-06 01:30:26 +0500

    Finished
    Added dark mode

--- Javascript/sneakers.js
+++ Javascript/sneakers.js
@@ -6,26 +6,82 @@ var sideNavBar = document.querySelector('.side-nav-bar');
 var sideNavBarExitBtn = document.querySelector('.side-nav-bar-inner-exit');
 var headerBtn = document.getElementById('headerBtn');
 var home = document.getElementById('home');
-var circleBG = 'blog-inner-box-controller-circle-click-background';
+var moon = document.querySelector('.fa-moon');
+var body = document.getElementsByTagName('BODY')[0];
+var pageOfImgSmall = document.querySelectorAll('#page_of_img')[0];
+var pageOfImgBig = document.querySelectorAll('#page_of_img')[1];
+var changeImgSmall = document.querySelectorAll('#change-img-small_size');
+var changeImgBig = document.querySelectorAll('#change-img-big_size');
+var blogImg = document.querySelector("#blog_img");
+var circleBackground = 'blog-inner-box-controller-circle-click-background';
 var checkBG = 'fa-check';
 var hide = 'hide';
+var circleClassId = -1;
+var resize = false;
+var customer = [
+    {
+        src: './images/customer.png',
+    },
+    {
+        src: './images/customer1.png',
+    },
+    {
+        src: './images/customer2.png',
+    },
+    {
+        src: './images/customer3.png',
+    },
+    {
+        src: './images/customer4.png',
+    },
+    {
+        src: './images/customer5.png',
+    }
+];
+moon.addEventListener('click', function (item) {
+    var _a, _b, _c;
+    if ((_a = body === null || body === void 0 ? void 0 : body.classList) === null || _a === void 0 ? void 0 : _a.contains('dark_mode'))
+        (_b = body === null || body === void 0 ? void 0 : body.classList) === null || _b === void 0 ? void 0 : _b.remove('dark_mode');
+    else
+        (_c = body === null || body === void 0 ? void 0 : body.classList) === null || _c === void 0 ? void 0 : _c.add('dark_mode');
+});
 circle.forEach(function (items) {
     items.addEventListener("click", function (item) {
         var _a, _b, _c, _d;
-        for (var i = 0; i < circle.length; i++)
-            (_b = (_a = circle[i]) === null || _a === void 0 ? void 0 : _a.classList) === null || _b === void 0 ? void 0 : _b.remove(circleBG);
-        (_d = (_c = item === null || item === void 0 ? void 0 : item.target) === null || _c === void 0 ? void 0 : _c.classList) === null || _d === void 0 ? void 0 : _d.add(circleBG);
+        var circle_class = item.target;
+        var id = parseInt(circle_class === null || circle_class === void 0 ? void 0 : circle_class.dataset["id"]);
+        if (id == circleClassId) {
+            (_a = circle_class === null || circle_class === void 0 ? void 0 : circle_class.classList) === null || _a === void 0 ? void 0 : _a.remove(circleBackground);
+            circleClassId = -1;
+        }
+        else {
+            circleClassId = parseInt(circle_class.dataset['id']);
+            for (var i = 0; i < circle.length; i++)
+                (_c = (_b = circle[i]) === null || _b === void 0 ? void 0 : _b.classList) === null || _c === void 0 ? void 0 : _c.remove(circleBackground);
+            (_d = circle_class === null || circle_class === void 0 ? void 0 : circle_class.classList) === null || _d === void 0 ? void 0 : _d.add(circleBackground);
+        }
     });
 });
 sideNavBarLinks.forEach(function (items) {
     items.addEventListener('click', function (item) {
         var _a;
+        var navBarlink = item.target;
         item.preventDefault();
-        var blockID = $(item.target).data("scroll");
+        var blockID = $(navBarlink).data("scroll");
         var elloffset = (_a = $(blockID).offset()) === null || _a === void 0 ? void 0 : _a.top;
         $("html, body").animate({
             scrollTop: elloffset - 90
         }, 700);
+    });
+});
+changeImgBig.forEach(function (items) {
+    items.addEventListener('click', function (item) {
+        imagePagination(pageOfImgBig, item);
+    });
+});
+changeImgSmall.forEach(function (items) {
+    items.addEventListener('click', function (item) {
+        imagePagination(pageOfImgSmall, item);
     });
 });
 checkBox === null || checkBox === void 0 ? void 0 : checkBox.addEventListener('click', function () {
@@ -42,8 +98,47 @@ sideNavBarExitBtn === null || sideNavBarExitBtn === void 0 ? void 0 : sideNavBar
     if (window.pageYOffset < 625)
         sideNavBar === null || sideNavBar === void 0 ? void 0 : sideNavBar.classList.add(hide);
 });
-window.addEventListener('scroll', function () {
-    if (window.pageYOffset > 625)
-        sideNavBar === null || sideNavBar === void 0 ? void 0 : sideNavBar.classList.remove(hide);
+window.addEventListener('scroll', function () { if (window.pageYOffset > 625)
+    sideNavBar === null || sideNavBar === void 0 ? void 0 : sideNavBar.classList.remove(hide); });
+window.addEventListener('load', function () { return window.innerWidth < 780 ? resize = true : resize = false; });
+window.addEventListener('resize', function () {
+    if (window.innerWidth < 780 && !resize) {
+        pageOfImgSmall.innerHTML = pageOfImgBig.innerHTML;
+        resize = true;
+    }
+    else if (window.innerWidth > 780 && resize == true) {
+        pageOfImgBig.innerHTML = pageOfImgSmall.innerHTML;
+        resize = false;
+    }
 });
+function imagePagination(imagePage, item) {
+    var _a, _b;
+    var getClass = item.target;
+    if ((_a = getClass === null || getClass === void 0 ? void 0 : getClass.classList) === null || _a === void 0 ? void 0 : _a.contains('fa-chevron-left')) {
+        var getPage = imagePage.innerText.charAt(1);
+        var pageNumber = parseInt(getPage);
+        var changePageNumber = pageNumber - 1;
+        if (changePageNumber <= 0) {
+            imagePage.innerHTML = "06 / 06";
+            blogImg['src'] = customer[customer.length - 1].src;
+        }
+        else {
+            imagePage.innerHTML = "0" + changePageNumber + " / 06";
+            blogImg['src'] = customer[changePageNumber - 1].src;
+        }
+    }
+    else if ((_b = getClass === null || getClass === void 0 ? void 0 : getClass.classList) === null || _b === void 0 ? void 0 : _b.contains('fa-chevron-right')) {
+        var getPage = imagePage.innerText.charAt(1);
+        var pageNumber = parseInt(getPage);
+        var changePageNumber = pageNumber + 1;
+        if (changePageNumber > 6) {
+            imagePage.innerHTML = "01 / 06";
+            blogImg['src'] = customer[0].src;
+        }
+        else {
+            imagePage.innerHTML = "0" + changePageNumber + " / 06";
+            blogImg['src'] = customer[changePageNumber - 1].src;
+        }
+    }
+}
 //# sourceMappingURL=sneakers.js.map
--- sneakers.ts
+++ sneakers.ts
@@ -1,31 +1,88 @@
-const circle = document.querySelectorAll('.blog-inner-box-controller-circle');
-const sideNavBarLinks = document.querySelectorAll('.side-nav-bar-inner-form-links');
-const checkBox = document.querySelector('.sign-up-accept-form_button');
-const sideNavBar = document.querySelector('.side-nav-bar');
-const sideNavBarExitBtn = document.querySelector('.side-nav-bar-inner-exit');
-const headerBtn = document.getElementById('headerBtn');
-const home: any = document.getElementById('home');
+const circle = document.querySelectorAll('.blog-inner-box-controller-circle') as NodeListOf<HTMLElement>;
+const sideNavBarLinks = document.querySelectorAll('.side-nav-bar-inner-form-links') as NodeListOf<HTMLElement>;
+const checkBox = document.querySelector('.sign-up-accept-form_button') as HTMLStyleElement;
+const sideNavBar = document.querySelector('.side-nav-bar') as HTMLStyleElement;
+const sideNavBarExitBtn = document.querySelector('.side-nav-bar-inner-exit') as HTMLStyleElement;
+const headerBtn = document.getElementById('headerBtn') as HTMLStyleElement;
+const home = document.getElementById('home') as HTMLStyleElement;
+const moon = document.querySelector('.fa-moon') as HTMLElement;
+const body = document.getElementsByTagName('BODY')[0] as HTMLStyleElement;
+const pageOfImgSmall = document.querySelectorAll('#page_of_img')[0] as HTMLStyleElement;
+const pageOfImgBig = document.querySelectorAll('#page_of_img')[1] as HTMLStyleElement;
+const changeImgSmall = document.querySelectorAll('#change-img-small_size') as NodeListOf<HTMLElement>;
+const changeImgBig = document.querySelectorAll('#change-img-big_size') as NodeListOf<HTMLElement>;
+const blogImg = document.querySelector("#blog_img") as HTMLImageElement;
 
-const circleBG: string = 'blog-inner-box-controller-circle-click-background';
+const circleBackground: string = 'blog-inner-box-controller-circle-click-background';
 const checkBG: string = 'fa-check';
 const hide: string = 'hide';
+let circleClassId: number = -1;
+let resize: boolean = false;
 
-circle.forEach((items: any) => {
-  items.addEventListener("click", (item: any) => {
-    for(let i = 0; i < circle.length; i++) circle[i]?.classList?.remove(circleBG);
-    item?.target?.classList?.add(circleBG);
+const customer = [
+  {
+    src: './images/customer.png',
+  },
+  {
+    src: './images/customer1.png',
+  },
+  {
+    src: './images/customer2.png',
+  },
+  {
+    src: './images/customer3.png',
+  },
+  {
+    src: './images/customer4.png',
+  },
+  {
+    src: './images/customer5.png',
+  }
+]
 
+moon.addEventListener('click', (item: Event) => {
+  if(body?.classList?.contains('dark_mode'))
+  body?.classList?.remove('dark_mode');
+
+  else body?.classList?.add('dark_mode');
+});
+
+circle.forEach((items: Node) => {
+  items.addEventListener("click", (item: Event) => {
+    const circle_class = item.target as HTMLStyleElement;
+    const id = parseInt(circle_class?.dataset["id"] as string)
+    
+    if(id == circleClassId) { circle_class?.classList?.remove(circleBackground); circleClassId = -1; }
+    
+    else {
+      circleClassId = parseInt(circle_class.dataset['id'] as string);
+      for(let i = 0; i < circle.length; i++) circle[i]?.classList?.remove(circleBackground);
+      circle_class?.classList?.add(circleBackground);
+    }
   });
 });
 
-sideNavBarLinks.forEach((items: any) => {
-  items.addEventListener('click', (item: any) => {
+sideNavBarLinks.forEach((items: Node) => {
+  items.addEventListener('click', (item: Event) => {
+    const navBarlink = item.target as HTMLStyleElement;
     item.preventDefault();
-    let blockID = $(item.target).data("scroll");
+    let blockID = $(navBarlink).data("scroll");
 		let elloffset: any = $(blockID).offset()?.top;
     $("html, body").animate({
 			scrollTop: elloffset - 90
 		}, 700);
+  });
+});
+
+changeImgBig.forEach((items: Node) => {
+  items.addEventListener('click', (item: Event) => {
+    imagePagination(pageOfImgBig, item);
+  });
+});
+
+changeImgSmall.forEach((items: Node) => {
+  items.addEventListener('click', (item: Event) => {
+    imagePagination(pageOfImgSmall, item);
   });
 });
 
@@ -42,7 +99,46 @@ sideNavBarExitBtn?.addEventListener('click', () => {
   if(window.pageYOffset < 625) sideNavBar?.classList.add(hide);
 });
 
-window.addEventListener('scroll', () => {
-  if(window.pageYOffset > 625) sideNavBar?.classList.remove(hide);
-});
+
+window.addEventListener('scroll', () => { if(window.pageYOffset > 625) sideNavBar?.classList.remove(hide) });
+
+window.addEventListener('load', () => window.innerWidth < 780 ? resize = true : resize = false);
+
+window.addEventListener('resize', () => {
+  if(window.innerWidth < 780 && !resize) { pageOfImgSmall.innerHTML = pageOfImgBig.innerHTML; resize = true; } 
+  else if(window.innerWidth > 780 && resize == true) { pageOfImgBig.innerHTML = pageOfImgSmall.innerHTML; resize = false; }
+  
+})
+
+
+function imagePagination(imagePage: HTMLElement, item:Event) {
+  const getClass = item.target as HTMLStyleElement;
+    if(getClass?.classList?.contains('fa-chevron-left')) {
+      const getPage = imagePage.innerText.charAt(1) as string;
+      const pageNumber = parseInt(getPage) as number;
+      const changePageNumber = pageNumber - 1 as number;
+      if(changePageNumber <= 0) { 
+        imagePage.innerHTML = `06 / 06`; 
+        blogImg['src'] = customer[customer.length - 1].src;
+      } 
+      else { 
+        imagePage.innerHTML = `0${changePageNumber} / 06`; 
+        blogImg['src'] = customer[changePageNumber-1].src;
+      }
+    }
+
+    else if(getClass?.classList?.contains('fa-chevron-right')) {
+      const getPage = imagePage.innerText.charAt(1) as string;
+      const pageNumber = parseInt(getPage) as number;
+      const changePageNumber = pageNumber + 1 as number;
+      if(changePageNumber > 6) { 
+        imagePage.innerHTML = `01 / 06`; 
+        blogImg['src'] = customer[0].src;
+      } 
+      else {
+        imagePage.innerHTML = `0${changePageNumber} / 06`;
+        blogImg['src'] = customer[changePageNumber-1].src;
+      }
+    }
+}
 
